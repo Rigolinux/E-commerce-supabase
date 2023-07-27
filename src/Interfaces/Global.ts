793b29427e6257213f6a093a673599ae01662c45
rigolinux <rigoberto7567@gmail.com>
2023-07-27 16:50:13 -0600

    add new interface for supbase

--- src/Interfaces/Global.ts
+++ src/Interfaces/Global.ts
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 declare interface Credentials {
     email?: string | undefined
     password?: string | undefined
     provider?: 'bitbucket' | 'github' | 'gitlab' | 'google' | undefined
+}
+
+declare interface UserStore {
+    user: object | null,
+    role: string | null
 }
