867dd32fb8ea286b00c6bee25dcee35faa6b33a0
rigolinux <rigoberto7567@gmail.com>
2023-10-09 15:26:34 -0600

    custom date picker

---
+++ src/modules/profile/components/CustomDatePicker.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import { ref } from 'vue';
+import DatePicker from 'vue-datepicker-next';
+  import 'vue-datepicker-next/index.css';
+
+  const time2 = ref(new Date());
+</script>
+
+<template>
+  <div >
+
+    <date-picker style="min-height: 100%;" v-model:value="time2" valueType="format"></date-picker>
+   
+  </div>
+</template>
