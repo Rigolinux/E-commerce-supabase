0a021e5768a87314480dbb8206f328d9704f61eb
rigolinux <rigoberto7567@gmail.com>
2023-10-09 15:54:49 -0600

    prfile data

--- src/modules/profile/views/ProfileView.vue
+++ src/modules/profile/views/ProfileView.vue
@@ -1,24 +1,128 @@
-<script setup lang="ts">
-import { UploadProfileImg } from '@/vueutils/UseBucket';
+v-col<script setup lang="ts">
+import useUser from '@/composables/useUser';
+
+import CustomDatePicker from '../components/CustomDatePicker.vue'
+import { ref, type Ref } from 'vue';
 
 
-const handleUpload = async (e: any) => {
-    const file = e.target.files[0];
-    const url = await UploadProfileImg(file);
-    //this path is be returned and must be saved in the database in a new table called user profile
-    console.log(url);
+const comp = useUser();
+
+const url = ref<string | null>(null);
+
+const image: Ref<Blob | null> = ref(null);
+
+
+//comp.GetUser('correo@gmail.com');
+
+const PreviewImage = (e:any) => {
+
+    console.log(e.target.files[0]);
+  url.value = URL.createObjectURL(e.target.files[0]);
 };
+
+const form = useUser().User;
+
 
 </script>
 
 <template>
 <div>
+    <v-row>
+        <v-col cols="12">
+            <h1>Informacion Personal</h1>
+        </v-col>
+    </v-row>
+    <v-row>
 
-    <h1>Upload a file</h1>
-    <input type="file" @change="handleUpload" />
+        <v-col cols="12" md="4" xl="4">
+            
+            <v-col cols="12" md="12" xl="12">
+            <div style="display: flex; justify-content: center; width: 80%;margin-left: 10%;max-height: 200px;">
+                <v-img
+                :src="url ? url : 'https://cdn.vuetifyjs.com/images/john.jpg'"
+                min-width="200"
+                ></v-img>
+            </div>
+            </v-col>        <v-file-input
+                        @change="PreviewImage"
+            accept="image/png, image/jpeg, image/bmp"
 
+            placeholder="Pick an avatar"
+            prepend-icon="mdi-camera"
+            label="Avatar"
+            ></v-file-input>
+        </v-col>
+        <v-col cols="12" md="8" xl="8">
+            <v-row>
+                <v-col cols="12" md="6" xl="6">
+                    <v-text-field
+                        v-model="form.nombre"
+                        label="Nombre"                       
+                        type="text"
+                        class="mx-auto max-w-[400px] bg-white"
+                    />
+                </v-col>
+                <v-col cols="12" md="6" xl="6">
+                    <v-text-field
+                        v-model="form.apellido"
+                        label="Apellido" 
+                        type="text"
+                        class="mx-auto max-w-[400px] bg-white"
+                    />
+                </v-col>
+            </v-row>
+            <v-row>
+                
+                    <v-col cols="12" md="6" xl="6">
+                    
+                        <v-text-field
+                            v-model="form.email"
+                            label="Correo Electronico"
+                            type="email"
+                            class="mx-auto max-w-[400px] bg-white"
+                            readonly
+                        />
+                    </v-col>
+                    <v-col cols="12" md="6" xl="6">
+                    
+                    <custom-date-picker  >
+                                </custom-date-picker>
+                    </v-col>
+            </v-row>
+            <v-row>
+                    <v-col cols="12" md="6" xl="6">
+                        <v-text-field
+                            v-model="form.direccion"
+                            label="Direccion"
+                            type="text"
+                            class="mx-auto max-w-[400px] bg-white"
+                        />
+                    </v-col>
+                    <v-col cols="12" md="6" xl="6">
+                        <v-autocomplete
+                            v-model="form.sexo"
+                            :items="['Masculino','Femenino']"
+                            label="Sexo"
+                        />
+                    </v-col>
+               
+            </v-row>
+        </v-col>
+    </v-row>
+    
+    
+    
+    
+                
 </div>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+
+.v-text-field:hover {
+   background-color: white;
+}
+</style>
+                 
+
---
+++ src/modules/profile/views/TestUploadView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { UploadProfileImg } from '@/vueutils/UseBucket';
+
+
+const handleUpload = async (e: any) => {
+    const file = e.target.files[0];
+    const url = await UploadProfileImg(file);
+    //this path is be returned and must be saved in the database in a new table called user profile
+    console.log(url);
+};
+
+</script>
+
+<template>
+<div>
+
+    <h1>Upload a file</h1>
+    <input type="file" @change="handleUpload" />
+
+</div>
+</template>
+
+
+<style scoped></style>
