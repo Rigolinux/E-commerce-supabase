4d4bf6b5f6d5fcf5d30c0b1727937ca006dbc9b6
rigolinux <rigoberto7567@gmail.com>
2023-10-10 18:12:14 -0600

    env and date picker

--- src/modules/profile/components/CustomDatePicker.vue
+++ src/modules/profile/components/CustomDatePicker.vue
@@ -1,15 +1,30 @@
+<!-- eslint-disable vue/no-mutating-props -->
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { defineProps, ref } from 'vue';
 import DatePicker from 'vue-datepicker-next';
   import 'vue-datepicker-next/index.css';
 
-  const time2 = ref(new Date());
+interface Props {
+  date? : Date | string | null;
+}
+
+
+const props = defineProps<Props>();
+console.log(props.date);
+const date =  ref<Date | null>(props.date);
+
+const onChange = (date: Date) => {
+  console.log(date);
+}
+  
 </script>
 
 <template>
   <div >
 
-    <date-picker style="min-height: 100%;" v-model:value="time2" valueType="format"></date-picker>
+    <date-picker style="min-height: 100%;" v-model:value="date"
+    @change="onChange"
+    valueType="format"></date-picker>
    
   </div>
 </template>
