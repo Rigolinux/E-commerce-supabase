8d129340615d45dc3d72a2a541eebb986c256c64
Kidito27 <69774201+Kidito27@users.noreply.github.com>
2023-10-23 10:59:39 -0600

    cambios visual state

--- src/components/sidebar/state.ts
+++ src/components/sidebar/state.ts
@@ -4,7 +4,10 @@ import { ref, computed } from 'vue'
 export const collapsed = ref(true)
 export const toggleSidebar = () => (collapsed.value = !collapsed.value)
 
-export const SIDEBAR_WIDTH = 180
+// export const SIDEBAR_WIDTH = 180
+// export const SIDEBAR_WIDTH_COLLAPSED = 60
+
+export const SIDEBAR_WIDTH = 275
 export const SIDEBAR_WIDTH_COLLAPSED = 60
 
 export const sidebarWidth = computed(
