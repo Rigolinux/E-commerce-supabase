653841d7ccf9e6c140dfcc48098a921dbdee503c
Kidito27 <69774201+Kidito27@users.noreply.github.com>
2023-11-07 14:11:10 -0600

    Solucionando Sidebar en App

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,39 @@
 // import Sidebar from '@/components/sidebar/Sidebar.vue';
 import AppSidebar from '../src/components/sidebar/Sidebar.vue';
 import { sidebarWidth } from '../src/components/sidebar/state';
+import { computed, ref } from 'vue';
+
 
 export default {
   components: {
     AppSidebar
   },
   setup() {
-    return { sidebarWidth }
+
+    // Una nueva ref para mantener el estado de visibilidad de la barra lateral
+    const isSidebarVisible = ref(false);
+
+    // Actualizar isSidebarVisible basado en el evento emitido
+    const updateSidebarVisibility = (isVisible: boolean) => {
+      isSidebarVisible.value = isVisible;
+    };
+
+    // La lógica para calcular el marginLeft
+    const marginLeft = computed(() => {
+      return isSidebarVisible.value ? sidebarWidth.value : '0px';
+    });
+
+    return { sidebarWidth, updateSidebarVisibility, marginLeft }
   }
 }
 
 </script>
 
 <template>
-  <AppSidebar />
-  <div :style="{ 'margin-left': sidebarWidth }">
+  <!-- <AppSidebar /> -->
+  <AppSidebar @sidebar-visibility="updateSidebarVisibility" />
+  <div :style="{ 'margin-left': marginLeft }">
+  <!-- <div :style="{ 'margin-left': sidebarWidth }"> -->
     <router-view />
   </div>
 </template>
